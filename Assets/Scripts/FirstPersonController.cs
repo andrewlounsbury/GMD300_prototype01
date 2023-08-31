79ab36bdc4892de4f8fdd3713a0b13b5440b078a
MTL\alounsbury <andrew.lounsbury@mymail.champlain.edu>
2023-08-31 17:37:49 -0400

    Created new scene to test first person controller and movement

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class FirstPersonController : MonoBehaviour
+{
+    public float MoveSpeed = 3;
+    public float RotationSpeed = 2500;
+    public InputActionAsset CharacterActionAsset;
+    public Camera FirstPersonCamera; 
+
+    private InputAction moveAction; 
+    private InputAction rotateAction;
+
+    private CharacterController characterController;
+
+    private Vector2 moveValue; 
+    private Vector2 rotateValue;
+    private Vector3 currentRotationAngle;
+
+    private void OnEnable()
+    {
+        CharacterActionAsset.FindActionMap("Gameplay").Enable(); 
+    }
+
+    private void OnDisable()
+    {
+        CharacterActionAsset.FindActionMap("Gameplay").Disable();
+    }
+    
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        moveAction = CharacterActionAsset.FindActionMap("Gameplay").FindAction("Move");
+        rotateAction = CharacterActionAsset.FindActionMap("Gameplay").FindAction("Rotate");
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false; 
+    }
+
+    void Update()
+    {
+        moveValue = moveAction.ReadValue<Vector2>() * MoveSpeed * Time.deltaTime;
+        rotateValue = rotateAction.ReadValue<Vector2>() * RotationSpeed * Time.deltaTime;
+
+        currentRotationAngle = new Vector3(currentRotationAngle.x - rotateValue.y, currentRotationAngle.y + rotateValue.x, 0);
+
+        FirstPersonCamera.transform.rotation = Quaternion.Euler(currentRotationAngle);
+
+        currentRotationAngle = new Vector3(Mathf.Clamp(currentRotationAngle.x, -85, 85), currentRotationAngle.y, currentRotationAngle.z);
+
+        characterController.Move(new Vector3(moveValue.x, 0, moveValue.y));
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Vector4 (0, 1, 1, 0.5f);
+        Gizmos.DrawSphere(transform.position, 0.5f); 
+    }
+}
