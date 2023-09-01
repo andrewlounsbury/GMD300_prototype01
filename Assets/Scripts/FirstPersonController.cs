232f92f9439d37bdf4795d16594ba03ce1abd073
MTL\alounsbury <andrew.lounsbury@mymail.champlain.edu>
2023-09-01 13:55:44 -0400

    Installed ProBuilder and attempted camera/player rotation fix... will try again l8r

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -9,7 +9,8 @@ public class FirstPersonController : MonoBehaviour
     public float MoveSpeed = 3;
     public float RotationSpeed = 2500;
     public InputActionAsset CharacterActionAsset;
-    public Camera FirstPersonCamera; 
+    public Camera FirstPersonCamera;
+    public Transform PlayerBody; 
 
     private InputAction moveAction; 
     private InputAction rotateAction;
@@ -41,17 +42,18 @@ public class FirstPersonController : MonoBehaviour
 
     void Update()
     {
+        //setting variables
         moveValue = moveAction.ReadValue<Vector2>() * MoveSpeed * Time.deltaTime;
         rotateValue = rotateAction.ReadValue<Vector2>() * RotationSpeed * Time.deltaTime;
 
+       //camera/player rotation
         currentRotationAngle = new Vector3(currentRotationAngle.x - rotateValue.y, currentRotationAngle.y + rotateValue.x, 0);
-
         FirstPersonCamera.transform.rotation = Quaternion.Euler(currentRotationAngle);
-
         currentRotationAngle = new Vector3(Mathf.Clamp(currentRotationAngle.x, -85, 85), currentRotationAngle.y, currentRotationAngle.z);
+        PlayerBody.Rotate(Vector3.up * rotateValue.x);
 
+        //movement stuff
         characterController.Move(new Vector3(moveValue.x, 0, moveValue.y));
-
     }
     private void OnDrawGizmos()
     {
